4b6fae60fe8411ce9a28f7595a7155664acddaaf
Kyle Rose <kylemail1946@gmail.com>
2026-07-11 16:06:04 -0700

    Added creation of JS-based gui folder structure.

--- project-gen.ps1
+++ project-gen.ps1
@@ -17,6 +17,9 @@ param(
     [Parameter(Mandatory=$false)]
     [string]$ProjectName,
 
+    [Parameter(Mandatory=$false)]
+    [switch]$HasGui,
+
     [switch]$Help
 )
 
@@ -43,3 +46,37 @@ New-Item -ItemType File -Path (Join-Path $TargetDirectory "__init__.py")
 New-Item -ItemType File -Path (Join-Path $TargetDirectory "main.py")
 New-Item -ItemType File -Path (Join-Path $TargetDirectory "requirements.txt")
 Copy-Item -Path (Join-Path $ScriptDirectory "_install.ps1") -Destination (Join-Path $TargetDirectory "install.ps1")
+
+function Make-Subfolder {
+    param(
+        [string]$TargetDirectory,
+        [string]$SubfolderName
+    )
+    {
+        $SubfolderPath = Join-Path $TargetDirectory $SubfolderName
+        if (-not (Test-Path $SubfolderPath)) {
+            New-Item -ItemType Directory -Path $SubfolderPath | Out-Null
+        }
+    }
+}
+
+
+if ($HasGui) {
+    New-Item -ItemType File -Path (Join-Path $TargetDirectory "gui.py")
+    Make-Subfolder -TargetDirectory $TargetDirectory -SubfolderName "root"
+    $GuiDirectory = Join-Path $TargetDirectory "root"
+    Make-Subfolder -TargetDirectory $GuiDirectory -SubfolderName "static"
+    Make-Subfolder -TargetDirectory $GuiDirectory -SubfolderName "templates"
+    $StaticDirectory = Join-Path $GuiDirectory "static"
+    $TemplatesDirectory = Join-Path $GuiDirectory "templates"
+    New-Item -ItemType File -Path (Join-Path $StaticDirectory "index.html")
+    New-Item -ItemType File -Path (Join-Path $TemplatesDirectory "index.html")
+    Make-Subfolder -TargetDirectory $TargetDirectory -SubfolderName "global"
+    $GlobalDirectory = Join-Path $TargetDirectory "global"
+    Make-Subfolder -TargetDirectory $GlobalDirectory -SubfolderName "css"
+    Make-Subfolder -TargetDirectory $GlobalDirectory -SubfolderName "js"
+    Make-Subfolder -TargetDirectory $GlobalDirectory -SubfolderName "images"
+    Make-Subfolder -TargetDirectory $TargetDirectory -SubfolderName "resources"
+    $ResourcesDirectory = Join-Path $TargetDirectory "resources"
+    Make-Subfolder -TargetDirectory $ResourcesDirectory -SubfolderName "images"
+}
