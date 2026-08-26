edce68f3b1cf7624fa7ce611e962105162be9935
Kyle Rose <kylemail1946@gmail.com>
2026-08-25 17:14:28 -0700

    change venv to .venv

--- _install.ps1
+++ _install.ps1
@@ -11,8 +11,8 @@ try {
 
 # Create virtual environment
 Write-Host "Creating virtual environment..."
-python3.14 -m venv venv
-.\venv\Scripts\Activate.ps1
+python3.14 -m venv .venv
+.\.venv\Scripts\Activate.ps1
 
 # Upgrade pip
 Write-Host "Upgrading pip..."
